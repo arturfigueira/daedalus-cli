16830a459950a1aab9278da873e54c243d5e8037
arturfigueira <45975680+arturfigueira@users.noreply.github.com>
2021-09-02 22:29:21 -0300

    ca cert unit tests

--- src/main/kotlin/com/github/daedalus/cli/connection/yaml/YamlConnectionProvider.kt
+++ src/main/kotlin/com/github/daedalus/cli/connection/yaml/YamlConnectionProvider.kt
@@ -14,6 +14,7 @@ import org.apache.http.ssl.SSLContexts
 import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.security.KeyStore
 import java.security.cert.Certificate
@@ -22,6 +23,7 @@ import javax.net.ssl.SSLContext
 
 
 class YamlConnectionProvider(configFilePath: String) : ConnectionProvider {
+  private val rootFolder: Path = Path.of(configFilePath).parent
   private val config: ElasticSearchConfig = ConfigLoader().loadConfigOrThrow(configFilePath)
 
   override fun provide(): RestHighLevelClient {
@@ -47,13 +49,11 @@ class YamlConnectionProvider(configFilePath: String) : ConnectionProvider {
     authorization: Authorization.PemAuthorization,
     httpHosts: List<HttpHost>
   ): RestHighLevelClient {
-    val caCertificatePath = Paths.get(authorization.caCertificate)
+    val caCertificate = resolveResourcePath(authorization.caCertificate)
 
     val factory = CertificateFactory.getInstance("X.509")
-
     var trustedCa: Certificate?
-
-    Files.newInputStream(caCertificatePath).use { `is` ->
+    Files.newInputStream(caCertificate).use { `is` ->
       trustedCa = factory.generateCertificate(`is`)
     }
 
@@ -129,6 +129,18 @@ class YamlConnectionProvider(configFilePath: String) : ConnectionProvider {
 
     return setupClient(httpHosts, setupCallback)
   }
+
+  private fun resolveResourcePath(resource : String) : Path{
+    val resourcePath = Paths.get(resource)
+    return if (resourcePath.isAbsolute) resourcePath else resolveFromClassPath(resource)
+  }
+
+  private fun resolveFromClassPath(resource: String) =
+    Path.of(
+      this::class.java.classLoader.getResource(
+        rootFolder.resolve(resource).toString()
+      )?.toURI()
+    )
 
   private fun setupClient(
     httpHosts: List<HttpHost>,
--- src/test/groovy/com/github/daedalus/cli/connection/yaml/YamlConnectionProviderSpec.groovy
+++ src/test/groovy/com/github/daedalus/cli/connection/yaml/YamlConnectionProviderSpec.groovy
@@ -1,10 +1,32 @@
 package com.github.daedalus.cli.connection.yaml
 
+import com.github.daedalus.cli.connection.ConnectionException
 import org.elasticsearch.client.RestClient
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class YamlConnectionProviderSpec extends Specification {
+
+    @Shared
+    def resourcePath = "/connection/yaml"
+
+    @Unroll
+    def "Wont provide when using incompatible authorization and scheme"() {
+        given:
+        def provider = new YamlConnectionProvider(configPath)
+
+        when:
+        provider.provide()
+
+        then:
+        thrown(ConnectionException)
+
+        where:
+        configPath << ["$resourcePath/invalid-basic-elastic.yml",
+                       "$resourcePath/invalid-pem-elastic.yml",
+                       "$resourcePath/invalid-pkcs-elastic.yml"]
+    }
 
     @Unroll
     def "Should provides a #label Rest Client from #configPath"() {
@@ -18,13 +40,14 @@ class YamlConnectionProviderSpec extends Specification {
         actualConnection != null
 
         where:
-        label                  | configPath
-        "Basic authentication" | "/connection/yaml/basic-elastic.yml"
+        label                    | configPath
+        "Basic authentication"   | "$resourcePath/basic-elastic.yml"
+        "CA Cert authentication" | "$resourcePath/cacert-elastic.yml"
     }
 
     def "Should configure multiple hosts"() {
         given:
-        def provider = new YamlConnectionProvider("/connection/yaml/basic-elastic.yml")
+        def provider = new YamlConnectionProvider("$resourcePath/basic-elastic.yml")
 
         when:
         def actualConnection = provider.provide()
